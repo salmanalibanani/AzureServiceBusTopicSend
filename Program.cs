a74dfabf5fd093e85ec6d4a0b3f80c59ab889772
Salman Ali Banani <salman.banani@purple.telstra.com>
2020-09-07 15:51:32 +1000

    First commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace AzureServiceBusTopicSend
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Extensions.Configuration;
+
+    class Program
+    {
+        const string connectionString = "<your_connection_string>";
+        const string TopicName = "demotopic4699";
+        static ITopicClient topicClient;
+
+        public static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+          
+            var connectionString = config["connectionString"];
+            topicClient = new TopicClient(connectionString, TopicName);
+
+            while (true)
+            {
+                Console.WriteLine("Type m to send a message to topic, q to quit");
+                var keyInfo = Console.ReadKey();  
+
+                if (keyInfo.KeyChar == 'm')
+                {
+                    Console.Write("\nMessage: ");
+                    var line = Console.ReadLine();
+                    await topicClient.SendAsync(new Message() { Body = Encoding.ASCII.GetBytes(line)});
+                }
+
+                if (keyInfo.KeyChar == 'q')
+                {
+                    break;
+                }
+            }  
+
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AzureServiceBusTopicSend
+
+Code sample.  See the accompanied blog <a href="https://salmanalibanani.com/2020/09/04/azure-service-bus-client-library-for-.net/">here</a>.
